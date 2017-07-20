4c9c3ab3e3ccc70f7f7b47ad61660dd3eb0b2b0f
E000542 <E000542@HW1579.cignitiglobal.com>
2017-07-20 15:50:06 +0530

    sampleproject

---
+++ src/SampleProject.java
@@ -0,0 +1,10 @@
+
+public class SampleProject {
+   public static void main(String[] args) {
+      // Prints "Hello World" in the terminal window.
+      System.out.println("Hello, World");
+      System.out.println("Hello, America");
+      System.out.println("Hello, India");
+      System.out.println("Hello, HYderabad");
+   }
+}
